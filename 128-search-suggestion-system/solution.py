b156cf1fc962425025bf8dbe1da310b393879c36
Akankhya Panigrahi <akankhyapanigrahi2005@gmail.com>
2026-06-27 12:58:22 +0530

    Create solution.py

---
+++ 128-search-suggestion-system/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        res = []
+        products.sort()
+
+        l, r = 0, len(products) - 1
+        for i in range(len(searchWord)):
+            c = searchWord[i]
+
+            # get the products with a matching prefix, using binary sort
+            while l <= r and (len(products[l]) <= i or products[l][i] != c):
+                l += 1
+            while l <= r and (len(products[r]) <= i or products[r][i] != c):
+                r -= 1
+            
+            res.append([])
+            remain = r - l + 1 # no. of words with a matching prefix
+            for j in range(min(3, remain)):
+                res[-1].append(products[l + j])
+        return res
+            
+            
+        
